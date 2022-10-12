abd6b3d71ea6133f4794009eb5b3064b7c4e729e
xuoliangXie123 <946561354@qq.com>
2022-10-12 14:57:35 +0800

    测试文件

---
+++ test.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# File       : test.py
+# Time       ：2022/10/12 14:53
+# Author     ：author name
+# version    ：python 3.6
+# Description：
+"""
+
+print("Hello Python")
